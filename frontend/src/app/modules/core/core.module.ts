cbd8eeb8a4217d8cc4dd21dba11fac6da438b324
Now <now.thongtangsai@gmail.com>
2023-04-20 22:38:14 +0700

    FE: remove the footer content and set the sidebar drawer effect

--- frontend/src/app/modules/core/core.module.ts
+++ frontend/src/app/modules/core/core.module.ts
@@ -3,18 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { FooterComponent } from './layout/footer/footer.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [
-    BaseLayoutComponent,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent,
-  ],
-  imports: [CommonModule, MatToolbarModule, MatIconModule],
+  declarations: [BaseLayoutComponent, HeaderComponent, SidebarComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatSidenavModule],
   exports: [BaseLayoutComponent],
 })
 export class CoreModule {}
