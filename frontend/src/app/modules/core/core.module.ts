d9ef30eec73a9082d8a898c0eda663456d2ad299
Now <now.thongtangsai@gmail.com>
2023-04-20 20:27:39 +0700

    FE: init the footer,header, and sidebar

--- frontend/src/app/modules/core/core.module.ts
+++ frontend/src/app/modules/core/core.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-  declarations: [BaseLayoutComponent],
+  declarations: [
+    BaseLayoutComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule],
   exports: [BaseLayoutComponent],
 })
