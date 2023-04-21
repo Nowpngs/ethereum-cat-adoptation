b7cfbab63d9f660f36889582eb4076204bbea9f7
Now <now.thongtangsai@gmail.com>
2023-04-21 21:49:07 +0700

    FE: adding the card UI

--- frontend/src/app/modules/cat/cat.module.ts
+++ frontend/src/app/modules/cat/cat.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
 import { CatRoutingModule } from './cat-routing.module';
 import { CatListsComponent } from './cat-lists/cat-lists.component';
 import { CatDetailsComponent } from './cat-details/cat-details.component';
 
 @NgModule({
   declarations: [CatListsComponent, CatDetailsComponent],
-  imports: [CommonModule, CatRoutingModule],
+  imports: [CommonModule, CatRoutingModule, MatCardModule],
 })
 export class CatModule {}
